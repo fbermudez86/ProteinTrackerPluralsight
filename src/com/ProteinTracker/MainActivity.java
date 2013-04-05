64355b70831e4d222568749b16fbb6d0147a9377
Jose Fabio Bermudez <fbermudez86@gmail.com>
2013-04-05 15:43:02 -0600

    Initial Commit

--- src/com/ProteinTracker/MainActivity.java
+++ src/com/ProteinTracker/MainActivity.java
@@ -1,16 +1,33 @@
 package com.ProteinTracker;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.app.DialogFragment;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.*;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+    //region mProgress properties
+    private static final int PROGRESS = 0x1;
+    private ProgressDialog mProgress;
+    private int mProgressStatus = 0;
+    private Handler handler = new Handler()
+    {
+        @Override
+        public void handleMessage(Message msg) {
+            mProgress.dismiss();
+        }
+    };
+    //endregion
 
     //region Listener properties
     private View.OnClickListener listener = new View.OnClickListener() {
@@ -19,14 +36,59 @@ public class MainActivity extends Activity {
             startActivity(new Intent(MainActivity.this, MainPreferenceActivity.class));
         }
     };
-    private View.OnClickListener resetButtonListener =  new View.OnClickListener() {
+
+    private View.OnClickListener resetButtonListener =  new View.OnClickListener()
+    {
         @Override
         public void onClick(View v) {
+            //Custom Dialog
+            Dialog dialog = new Dialog(MainActivity.this);
+            dialog.setContentView(R.layout.custom_dialog);
+            dialog.setTitle("Custom Dialog");
+            TextView textView =  (TextView)dialog.findViewById(R.id.cdText);
+            textView.setText("Hello");
+            dialog.show();
+            /*
             DialogFragment confirmResetDialogFragment = ConfirmResetDialogFragment.newInstance(R.string.resetDialogMessage);
-            confirmResetDialogFragment.show(getFragmentManager(), "dialog");
-        }
-    };
+            confirmResetDialogFragment.show(getFragmentManager(), "dialog");*/
+
+            /*mProgress = new ProgressDialog(v.getContext());
+            mProgress.setCancelable(true);
+            mProgress.setMessage("Doing Something...");
+            mProgress.setTitle("Waiting");
+            mProgress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            mProgress.setProgress(0);
+            mProgress.setMax(10);
+            mProgress.show();
+
+            //reset progress bar status
+
+            //ProgressBar initialization
+            // mProgress = (ProgressBar) findViewById(R.id.progressBar);
+            // Start lengthy operation in a background thread
+            new Thread(new Runnable() {
+                public void run() {
+                    try
+                    {
+                        Thread.sleep(10000);
+                    }
+                    catch (InterruptedException e)
+                    {
+                        e.printStackTrace();
+                    }
+                    // Update the progress bar
+                    handler.post(new Runnable() {
+                        public void run() {
+                            mProgress.setProgress(mProgressStatus);
+                        }
+                    });
+                    handler.sendEmptyMessage(0);
+                }
+            }).start();
+        }*/
+    }};
     //endregion
+
 
     /**
      * Called when the activity is first created.
